5e78aa16ce789dc5cbe616695658d37c0a440b98
sananeo0 <sananeo@code.edu.az>
2023-08-28 07:39:18 +0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualBasic;
+using System.Diagnostics.Metrics;
+
+namespace SecondTask
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int vowels = 0;
+            int consonants = 0;
+            string input = "HEllo world";
+            string lowLetter = input.ToLower();
+            char[] vowelsChar = { 'a', 'e', 'i', 'o', 'u' };
+          
+            foreach(char c in lowLetter)
+            {
+                if (char.IsLetter(c))
+                {
+
+                    if ("aeiou".Contains(c))
+                    {
+                        vowels++;
+                    }
+                    else
+                    {
+                        consonants++;
+                    }
+                }
+            }
+            Console.WriteLine($"You have {vowels} vowels and {consonants} consonants in your word.") ;
+        }
+    }
+}
